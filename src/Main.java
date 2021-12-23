07bb70dbdb054301e81a76f64386746b7784f478
Ilya Maximov <xc.navi@gmail.com>
2021-12-23 14:29:53 +0200

    v3

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
 
+        System.out.println("введите число");
+      String  num = scanner.nextLine();
+String x ="";
+if (num.length()<=3){x = num;} else {
+    for (int i = 3; i <= num.length(); i+=3) {
+
+        x = x + num.substring(i-3, i) + ",";
+
+    }
+}
+        System.out.println(x);
     }
 }
