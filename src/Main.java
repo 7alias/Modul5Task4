4b6daa9e577a284cb61d01456a78989c1bce7123
Ilya Maximov <xc.navi@gmail.com>
2021-12-23 15:16:45 +0200

    end

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
         System.out.println("введите число");
-      String  num = scanner.nextLine();
-String x ="";
-if (num.length()<=3){x = num;} else {
-    for (int i = 3; i <= num.length(); i+=3) {
-
-        x = x + num.substring(i-3, i) + ",";
-
+        int myInt = scanner.nextInt();
+        DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
+        DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
+        symbols.setGroupingSeparator(','); //разделитель тысяч: ,
+        formatter.setDecimalFormatSymbols(symbols);
+        System.out.println(formatter.format(myInt));
     }
 }
-        System.out.println(x);
-    }
-}
+
