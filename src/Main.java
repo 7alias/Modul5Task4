7c6dbde293088f0fe0c19da06d4f3bbb3ec25570
Ilya Maximov <xc.navi@gmail.com>
2021-12-23 15:44:57 +0200

    commit4

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-        //System.out.println("введите число");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите число");
 
-        //int myInt = scanner.nextInt();
-        int myInt = 12345678;
+        int myInt = scanner.nextInt();
+        //int myInt = 12345678;
 
         DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance();
         DecimalFormatSymbols symbols = formatter.getDecimalFormatSymbols();
