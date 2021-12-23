9cbdd120a26ce1c1cc71548c7fe550fa3dbc2ea4
Ilya Maximov <xc.navi@gmail.com>
2021-12-23 13:40:56 +0200

    v3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("text");
-        System.out.println("text");
-        System.out.println("text");
-        System.out.println("text");
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("text2");
 
     }
 }
